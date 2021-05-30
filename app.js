b69ff68c2ae53c540c438e1e835bc27c5664a717
Himanshu malik <himanshumalik552@gmail.com>
2021-05-30 18:49:26 +0530

    backend connect seccessful

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const passport = require('passport');
+const dotenv  = require('dotenv');
+const morgan = require('morgan');
+const session = require('express-session')
+const exphbs = require('express-handlebars');
+const connectDB = require('./config/db');
+
+
+//Load config
+
+dotenv.config({path: './config/config.env'})
+
+//Passport config
+
+require('./config/passport')(passport)
+
+
+connectDB();
+
+const app = express();
+//Logging 
+if(process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'));
+}
+
+//Handlebar
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Session
+app.use(session({
+    secret:'keyboard cat',
+    resave: false,
+    saveUninitialized:false
+}))
+
+//passport middleware
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Router
+
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+
+//Static folder
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+//@desc LoginLanding page
+//@route GET /
+
+router.get('/', (req, res) =>{
+    res.render('login', {
+        layout: 'login',
+    });
+})
+
+//@desc Dashboard
+//@route GET/ Dashboard
+
+router.get('/dashboard', (req, res)=>{
+   res.render('dashboard')
+})
+
+module.exports = router
