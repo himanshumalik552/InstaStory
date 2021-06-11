853032e7ac8ae058690a6b5fafce2235c31ec7fa
HimanshuMalikHSV <himanshu.malik@hsv.digital>
2021-06-11 20:29:38 +0530

    OK

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -9,14 +9,14 @@ module.exports = {
     if (str.length > len && str.length > 0) {
       let new_str = str + " ";
       new_str = str.substr(0, len);
-      new_str = str.substr(0, new_str.lastIndexOf(" "));
-      new_str = new_str.length > 0 ? new_str : str.substr(0, len);
-      return new_str + "...";
+      new_str = str.substr(0, new_str.lastIndexOf(' '))
+      new_str = new_str.length > 0 ? new_str : str.substr(0, len)
+      return new_str + '...';
     }
     return str;
   },
   stripTags: function (input) {
-    return input.replace(/<(?:.|\n)*?>/gm, "");
+    return input.replace(/<(?:.|\n)*?>/gm, '');
   },
 
   editIcon: function (storyUser, loggedUser, storyId, floating = true) {
